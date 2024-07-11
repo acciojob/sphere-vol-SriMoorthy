50add25ffe6f95dd3374817716546c6906762f1e
SriMoorthy <srimoorthy27@gmail.com>
2024-07-11 16:42:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function volume_sphere() {
     //Write your code here
 	let R = document.getElementById('radius').value;
-	let vol = (3/4) * Math.PI * Math.pow(R,3)
+	let vol = (4/3) * Math.PI * Math.pow(R,3)
 	// document.getElementById('volume').value = vol
 	if(R>0){
-		document.getElementById('volume').value = vol
+		document.getElementById('volume').value = vol.toFixed(4)
 	}
 	else{
 		document.getElementById('volume').value = NaN
@@ -13,4 +13,4 @@ function volume_sphere() {
   
 } 
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+// window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
