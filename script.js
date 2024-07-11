69e3a0f90aeb1d6537259fddc48a49801aeb37c0
SriMoorthy <srimoorthy27@gmail.com>
2024-07-11 16:28:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function volume_sphere() {
     //Write your code here
+	let R = document.getElementById('radius').value;
+	let vol = (3/4) * Math.PI * Math.pow(R,3)
+	// document.getElementById('volume').value = vol
+	if(R>0){
+		document.getElementById('volume').value = vol
+	}
+	else{
+		document.getElementById('volume').value = NaN
+	}
+	
   
 } 
 
